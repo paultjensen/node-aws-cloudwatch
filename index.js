0ed81bc861fac0e1b8a439128d00fd300849f823
paul <paul.t.jensen@gmail.com>
2019-01-06 20:19:42 -0600

    Added code to get log events.

--- index.js
+++ index.js
@@ -12,7 +12,18 @@
     let Config = require('./lib/config').config;
     let Logger = require('./lib/logger');
 
-    Cloudwatch.initLogs(Config);
+    Cloudwatch.initLog(Config);
 
+    Cloudwatch.describeLogStreams(Config.cloudwatch_log_group_name).then((logStreamAry) => {
+        Logger.log.info('Found log streams: ' + JSON.stringify(logStreamAry));
+        let nowTs = new Date().getTime();
+        let thenTs = nowTs - 10000;
+        return Cloudwatch.getLogEvents(Config.cloudwatch_log_group_name, logStreamAry,
+            thenTs, nowTs, null);
+    }).then((result) => {
+        Logger.log.info('Found log events: ' + JSON.stringify(result));
+    }).catch((err) => {
+        Logger.log.info(JSON.stringify(err));
+    });
 
 })();
